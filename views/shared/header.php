6335f388ba3d306f9d8693a6f051cd85ae00e12a
MartinaMax <mmaximova37@gmail.com>
2023-12-08 20:40:18 +0100

    Header

--- views/shared/header.php
+++ views/shared/header.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
         <link rel="stylesheet" href="/CapWizards/assets/css/main.css">
-        <script src="/assets/amountCounter.js" defer></script>
+        <script src="/CapWizards/assets/amountCounter.js" defer></script>
         <title>Document</title>
     </head>
     <body>
@@ -84,3 +84,75 @@
   </div>
 </nav>  
 </header>
+
+<!-- <header>
+        <nav class="d-flex justify-content-between">
+            <!-- Alcoholic 
+            <div class="d-flex">
+                <div class="dropdown d-flex gap-30">
+                        <h2 class="dropdown h2-small gap-5" id="dropdownMenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Alcoholic</h2>
+                        <img src="/CapWizards/assets/svg/arrow.svg" alt="Arrow icon">
+                    <div class="dropdown-menu border-0 rounded-0 p-0 " aria-labelledby="dropdownMenu">
+                        <div class="d-flex">
+                            <a href="http://localhost/CapWizards/Products/Alcoholic/Cider" class="dropdown-item text-center p-0">
+                                <div class="menu-red"> 
+                                    <img src="/CapWizards/assets/svg/cider.svg" alt="Cider icon">
+                                    <h2 class="h2-small h2-bage">Cider</h2>
+                                </div>
+                            </a>
+                            <a href="http://localhost/CapWizards/Products/Alcoholic/Beer" class="dropdown-item text-center p-0">
+                                <div class="menu-yellow">
+                                    <img src="/CapWizards/assets/svg/beer-bage.svg" alt="Beer icon">
+                                    <h2 class="h2-small h2-bage">Beer</h2>
+                                </div>
+                            </a>
+                            <a href="http://localhost/CapWizards/Products/Alcoholic/Shaker" class="dropdown-item text-center p-0">
+                                <div class="menu-blue"> 
+                                    <img src="/CapWizards/assets/svg/mixer.svg" alt="Shaker icon">
+                                    <h2 class="h2-small h2-bage">Shaker</h2>
+                                </div>
+                            </a>
+                            <a href="http://localhost/CapWizards/Products/Alcoholic/Wine" class="dropdown-item text-center p-0">
+                                <div class="menu-pink"> 
+                                    <img class="margin-5" src="/CapWizards/assets/svg/vine.svg" alt="Wine icon">
+                                    <h2 class="h2-small h2-bage">Wine</h2>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            <!-- Non-alcoholic
+                <div class="dropdown d-flex">
+                    <h2 class="dropdown h2-small gap-5" id="dropdownMenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Non-Alcoholic</h2>
+                    <img src="/CapWizards/assets/svg/arrow.svg" alt="Arrow icon">
+                <div class="dropdown-menu border-0 rounded-0 p-0 " aria-labelledby="dropdownMenu">
+                    <div class="d-flex">
+                        <a class="dropdown-item text-center p-0">
+                            <div class="menu-red"> 
+                                <img src="/CapWizards/assets/svg/cider.svg" alt="Soda icon">
+                                <h2 class="h2-small h2-bage">Soda</h2>
+                            </div>
+                        </a>
+                        <a href="" class="dropdown-item text-center p-0">
+                            <div class="menu-yellow">
+                                <img src="/CapWizards/assets/svg/beer-bage.svg" alt="Soft drink icon">
+                                <h2 class="h2-small h2-bage">Soft drink</h2>
+                            </div>
+                        </a>
+                        <a class="dropdown-item text-center p-0">
+                            <div class="menu-blue"> 
+                                <img src="/CapWizards/assets/svg/mixer.svg" alt="Water icon">
+                                <h2 class="h2-small h2-bage">Water</h2>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            </div>
+        <!-- Icons 
+            <div>
+                <img class="gap-30" src="/CapWizards/assets/svg/cart.svg" alt="Cart icon">
+                <img src="/CapWizards/assets/svg/avatar.svg" alt="Admin icon">
+            </div>
+            </nav>
+        </header>  -->

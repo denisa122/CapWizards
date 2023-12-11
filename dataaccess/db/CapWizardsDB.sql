6729a9fdfc749b15c7cf87df1499645de286a245
AccountForStudying <10407739@ucn.dk>
2023-12-11 14:51:00 +0100

    Update CapWizardsDB.sql

--- dataaccess/db/CapWizardsDB.sql
+++ dataaccess/db/CapWizardsDB.sql
@@ -81,13 +81,8 @@ CREATE TABLE Customer (
     email VARCHAR(80),
     phoneNumber varchar(20) NULL,
     userName VARCHAR(50),
-<<<<<<< HEAD
-    password VARCHAR(255),
-    role VARCHAR(50)
-=======
     FK_accountID INT,
     FOREIGN KEY (FK_accountID) REFERENCES Account (accountID)
->>>>>>> parent of ac2174a (account table)
 )ENGINE = InnoDB;
 
 CREATE TABLE Address (

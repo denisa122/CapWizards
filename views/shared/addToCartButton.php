2897470f1c8057a717c6712e1eb6af697ef2dd89
AccountForStudying <10407739@ucn.dk>
2023-12-12 17:42:50 +0100

    Destroying the cart cuz i simply don't know how to fix it.

--- views/shared/addToCartButton.php
+++ views/shared/addToCartButton.php
@@ -4,7 +4,9 @@ require_once("../../dataaccess/db/DBConnector.php");
 
 
 $customerID = $_SESSION['customerID'];
-$orderID = $_SESSION['orderID'];
+$orderID = isset($_SESSION['orderID']) ? $_SESSION['orderID'] : null;
+
+echo $customerID;
 
 if (isset($_POST['add_to_cart'])) {
     $productID = $_POST['productID'];
@@ -14,19 +16,59 @@ if (isset($_POST['add_to_cart'])) {
     $order_exists->bind_param("s", $customerID);
     $order_exists->execute();
     $resultOrder = $order_exists->get_result();
+   
     
-    if ($resultOrder->num_rows === 0) {
-        // If no order exists, create a new order for the customer
-        $orderID = create_unique_id(); // Generate a unique order ID
-        $insert_order = $conn->prepare("INSERT INTO `Order` (orderID, FK_customerID) VALUES (?, ?)");
-        $insert_order->bind_param("ss", $orderID, $customerID);
-        $insert_order->execute();
-    } else {
-        // If an order exists, fetch the orderID
-        $existing_order = $resultOrder->fetch_assoc();
-        $orderID = $existing_order['orderID'];
+    // if ($resultOrder->num_rows === 0) {
+    //     // If no order exists, create a new order for the customer
+    //     $orderID = $_SESSION['orderID']; // Generate a unique order ID
+    //     $insert_order = $conn->prepare("INSERT INTO `Order` (orderID, FK_customerID) VALUES (?, ?)");
+    //     $insert_order->bind_param("ss", $orderID, $customerID);
+    //     $insert_order->execute();
+    // } else {
+    //     // If an order exists, fetch the orderID
+    //     $existing_order = $resultOrder->fetch_assoc();
+    //     $orderID = $existing_order['orderID'];
+    // }
+
+    if ($orderID === null) {
+        $order_exists = $conn->prepare("SELECT * FROM `Order` WHERE FK_customerID = ?");
+        $order_exists->bind_param("s", $customerID);
+        $order_exists->execute();
+        $resultOrder = $order_exists->get_result();
+
+        if ($resultOrder->num_rows === 0) {
+            // If no order exists, create a new order for the customer
+            $orderID = create_unique_id(); // Create a unique order ID
+            $_SESSION['orderID'] = $orderID; // Store the orderID in the session
+            $insert_order = $conn->prepare("INSERT INTO `Order` (orderID, FK_customerID) VALUES (?, ?)");
+            $insert_order->bind_param("ss", $orderID, $customerID);
+            $insert_order->execute();
+        } else {
+            // If an order exists, fetch the orderID
+            $existing_order = $resultOrder->fetch_assoc();
+            $orderID = $existing_order['orderID'];
+        }
+
     }
-    
+
+
+    // if ($resultOrder->num_rows === 0) {
+    //     // If no order exists, create a new order for the customer
+    //     echo "Customer doesn't exist!";
+    //     if ($orderID === null) {
+    //         $orderID = create_unique_id(); // Create a unique order ID
+    //         $_SESSION['orderID'] = $orderID; // Store the orderID in the session
+    //     }
+        
+    //     $insert_order = $conn->prepare("INSERT INTO `Order` (orderID, FK_customerID) VALUES (?, ?)");
+    //     $insert_order->bind_param("ss", $orderID, $customerID);
+    //     $insert_order->execute();
+    // } else {
+    //     // If an order exists, fetch the orderID
+    //     $existing_order = $resultOrder->fetch_assoc();
+    //     $orderID = $existing_order['orderID'];
+    // }
+
     
     $verify_cart = $conn->prepare("SELECT * FROM ProductOrder WHERE productID = ? AND orderID = ?");
     $verify_cart->bind_param("ss", $productID, $orderID);
--- views/shared/session.php
+++ views/shared/session.php
@@ -11,20 +11,26 @@ session_set_cookie_params([
 
 session_start();// Start the session if not already started
 function create_unique_id() {
-    return md5(uniqid(rand(), true));
+    $random_number = mt_rand(10000, 99999);
+    
+    // Append a unique identifier using uniqid()
+    $unique_id = $random_number . uniqid();
+
+    // Return the generated numeric ID
+    return $unique_id;
 }
 
 if (!isset($_SESSION['customerID'])) {
     // Generate a unique customer ID and store it in the session
     $_SESSION['customerID'] = create_unique_id(); // Replace with your logic to generate an ID
 }
-
-if (!isset($_SESSION['orderID'])) {
-    // Generate a unique order ID and store it in the session
-    $_SESSION['orderID'] = create_unique_id(); // Replace with your logic to generate an ID
-}
-
 $customerID = $_SESSION['customerID'];
-$orderID = $_SESSION['orderID'];
+// if (!isset($_SESSION['orderID'])) {
+//     // Generate a unique order ID and store it in the session
+//     $_SESSION['orderID'] = create_unique_id(); // Replace with your logic to generate an ID
+// }
+
+
+// $orderID = $_SESSION['orderID'];
 
 ?>
